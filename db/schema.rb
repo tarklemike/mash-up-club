e8e9127757d0dbeecd3ce676c18e77efa55c9bba
Mike Tarkleman <miketarkleman@gmail.com>
2024-08-01 00:12:16 +0100

    rollback migrations & create devise User migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_224839) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ideas", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.text "description"
-    t.string "audio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_ideas_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "image"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "ideas", "users"
 end
